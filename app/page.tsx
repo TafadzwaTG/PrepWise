4c4bcf996b9fe20735fde463d5d102756ce0c474
Tafadzwa McNorman <gumbitafadzwa@gmail.com>
2025-05-16 15:47:33 +0200

    Initial project setup with Next.js, TypeScript, Tailwind and custom assets

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Mock Interviews</h1>
+      <p>Welcome to the Mock Interviews page!</p>
+    </div>
+  )
+}
+export default Page
