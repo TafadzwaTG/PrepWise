fc214ed8078eb9ae340e0d03b8d4dd5f60868f8c
Tafadzwa McNorman <gumbitafadzwa@gmail.com>
2025-05-19 17:23:48 +0200

    Add auth layout with sign-in and sign-up pages

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+import React, {ReactNode}from 'react'
+
+const Layout = ({children}: {children: React.ReactNode})=> {
+  return (
+    <div>
+      <h1> Layout</h1>
+    </div>
+  )
+}
+export default Layout
